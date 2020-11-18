229ad887af27bc8c185124eb1d8348ba3dcb0515
kitamuratoshimi <kt.marr49@gmail.com>
2020-11-18 11:44:49 +0900

    Update vue.php

--- src/test/vue.php
+++ src/test/vue.php
@@ -30,15 +30,18 @@ include($inc . "header.php");
       <p>宣言レンダリング<code v-pre>{{ testMessage }}</code>を使って、宣言レンダリングで表示してみよう。</p>
       <pre v-pre>&lt;p&gt;{{ testMessage }}&lt;/p&gt;</pre>
     </base-wrapper>
-
     <base-wrapper tag="article">
+      <p>{{ testMessage }}</p>
+
       <h3>課題2：オブジェクトのループ</h3>
       <p>オブジェクトデーター<code v-pre>testObject</code>を使って、繰り返しをしてみよう。</p>
       <pre v-pre>&lt;ul&gt;
   &lt;li v-for=&quot;(value, key) in testObject&quot;&gt;{{key}}: {{value}}&lt;/li&gt;
 &lt;/ul&gt;</pre>
     </base-wrapper>
-
+    <ul>
+      <li v-for="(value, key) in testObject">{{key}}: {{value}}</li>
+    </ul>
     <base-wrapper tag="article">
       <h3>課題3：配列のループ</h3>
       <p>配列データー<code>testArray</code>を使って、繰り返しをしてみよう。</p>
@@ -46,13 +49,15 @@ include($inc . "header.php");
   &lt;li v-for=&quot;value in testArray&quot;&gt;{{value}}&lt;/li&gt;
 &lt;/ul&gt;</pre>
     </base-wrapper>
-
+    <ul>
+      <li v-for="value in testArray">{{value}}</li>
+    </ul>
     <base-wrapper tag="article">
       <h3>課題4：オープンデーター（大量の情報）の取得</h3>
       <p>オープンデーター<code v-pre>openData</code>を、宣言レンダリングで表示しててみよう。</p>
       <pre v-pre>&lt;pre&gt;{{openData}}&lt;/pre&gt;</pre>
     </base-wrapper>
-
+    <pre>{{openData}}</pre>
     <base-wrapper tag="article">
       <h3>課題5: オープンデーター(大量の情報)のループ</h3>
       <p>オープンデーター<code v-pre>openData</code>を、ループで表示しててみよう。</p>
@@ -60,7 +65,9 @@ include($inc . "header.php");
   &lt;li v-for=&quot;value in openData&quot;&gt;{{value}}&lt;/li&gt;
 &lt;/ul&gt;</pre>
     </base-wrapper>
-
+    <ul>
+      <li v-for="value in openData">{{value}}</li>
+    </ul>
     <base-wrapper tag="article">
       <h3>課題6: オープンデーター(大量の情報)の表示をコントロール</h3>
       <p>オープンデーター<code>openData</code>を使って、ループした情報から、宣言レンダリングで<b>名称/種別/所在地だけ</b>を表示してみよう。</p>
@@ -74,6 +81,15 @@ include($inc . "header.php");
   &lt;/li&gt;
 &lt;/ul&gt;</pre>
     </base-wrapper>
+    <ul>
+      <li v-for="value in openData">
+        <h4>{{ value['名称'] }}</h4>
+        <ul>
+          <li>種別: {{ value['種別'] }}</li>
+          <li>所在地: {{ value['所在地'] }}</li>
+        </ul>
+      </li>
+    </ul>
 
     <base-wrapper tag="article">
       <h3>課題7: オープンデーター（大量の情報）をレイアウトする</h3>
